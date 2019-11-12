931cdae1ab6e1455b31e139245936412adf3657e
victor politano <viant3ucf@gmail.com>
2019-11-12 17:02:21 -0500

    changed route in index.js to /search

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -8,7 +8,7 @@ function Nav() {
 
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <a className="nav-link" href="/">Search</a>
+          <a className="nav-link" href="/search">Search</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/saved">Saved</a>
