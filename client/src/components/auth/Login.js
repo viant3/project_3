c5d4f9e4f0d04450c28d9537a4ac63df51680217
victor politano <viant3ucf@gmail.com>
2019-11-12 16:07:05 -0500

    heroku

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -15,7 +15,7 @@ class Login extends Component {
   }
 componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
-      this.props.history.push("/dashboard"); // push user to dashboard when they login
+      this.props.history.push("/dashboard"); // push user to dashboard when they login.
     }
 if (nextProps.errors) {
       this.setState({
