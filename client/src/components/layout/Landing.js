9e45c885d5698ba964adf31d813a0c879c2ee366
victor politano <viant3ucf@gmail.com>
2019-11-13 12:23:33 -0500

    added img to landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,7 +5,7 @@ import "./style.css";
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper " >
+      <div style={{ height: "75vh" }} className="container valign-wrapper matrix" >
         <div className="row matrix">
           <div className="col s12 center-align">
             <br />
