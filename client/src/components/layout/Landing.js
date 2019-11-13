70a29b857c70b63f8cc573fc2dd08cc606010aad
victor politano <viant3ucf@gmail.com>
2019-11-13 12:00:40 -0500

    added img to landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
+
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div style={{ height: "75vh" }} className="container valign-wrapper matrix">
         <div className="row">
           <div className="col s12 center-align">
-            <h4>
-             
-            </h4>
+        
             <p className="flow-text grey-text text-darken-1">
              
             </p>
