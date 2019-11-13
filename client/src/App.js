efce752f04ca0cd685bbb2168cad162236969cc4
victor politano <viant3ucf@gmail.com>
2019-11-13 09:33:11 -0500

    replaced navbar in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Router>
-          <div>
+          <div className="App">
             <Switch>
               <Route exact path="/search" component={Search} />
               <Route exact path="/saved" component={Saved} />
@@ -51,7 +51,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
 
-  
+              <Navbar />
               <Landing />
             </Switch>
             <Switch>
