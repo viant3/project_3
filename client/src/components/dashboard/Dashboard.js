5242164d3884f0576f81bc0503e6c27a93c40caa
victor politano <viant3ucf@gmail.com>
2019-11-12 20:31:00 -0500

    trying a navbar in dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
-import Nav from "./components/Nav";
+import Nav from "./Nav";
 
 class Dashboard extends Component {
   onLogoutClick = e => {
