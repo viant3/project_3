cd67880409ca56c7e82737c642f7fc3f2804ed58
victor politano <viant3ucf@gmail.com>
2019-11-14 11:41:05 -0500

    using imges within divs as backgrounds using css for placement

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -90,7 +90,7 @@ class Search extends Component {
       <Nav />
       <Dashboard />
         <Row>
-      <div style={{ height: "35vh", width: "50vw"}} className="nsa"></div><div style={{ height: "35vh", width: "50vw"}} className="dod"></div>
+      <div style={{ height: "35vh", width: "30vw"}} className="nsa"></div><div style={{ height: "35vh", width: "30vw"}} className="dod"></div>
         </Row>
         <Row>
           <div className="col rounded bg-light mb-4 mt-4 p-4">
