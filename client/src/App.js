c28486b6bc41b34b8bd606050980b8fbd2c72e61
victor politano <viant3ucf@gmail.com>
2019-11-14 13:36:27 -0500

    changed login route

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ if (localStorage.jwtToken) {
     // Logout user
     store.dispatch(logoutUser());
     // Redirect to login
-    window.location.href = "./login";
+    window.location.href = "./Register";
   }
 }
 
