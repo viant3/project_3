f0afbbcdfd5c2cfae5cf6044942f320194c951dc
victor politano <viant3ucf@gmail.com>
2019-11-14 12:06:06 -0500

    removed Col.. back to divs

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -90,7 +90,7 @@ class Search extends Component {
       <Nav />
       <Dashboard />
         <Row>
-      <div style={{ height: "35vh", width: "40vw"}} className="nsa"></div><div style={{ height: "35vh", width: "35vw"}} className="dod"></div>
+      <div style={{ height: "35vh", width: "40vw"}} className="nsa"></div>
         </Row>
         <Row>
           <div className="col rounded bg-light mb-4 mt-4 p-4">
