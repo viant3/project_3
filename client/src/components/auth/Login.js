3fa239635d1a78a6e906b8fca6116e9c685fe157
victor politano <viant3ucf@gmail.com>
2019-11-15 15:32:58 -0500

    changed logout button to link imported link from react-router-dom in dashboard

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -90,13 +90,13 @@ return (
               <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                 <button
                   style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
+                    width: "100px",
+                    borderRadius: "1px",
+                    letterSpacing: "1px",
                     marginTop: "1rem"
                   }}
                   type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                  className="btn waves-effect waves-light hoverable gray accent-3"
                 >
                   Login
                 </button>
