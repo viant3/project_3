5b9487af19b9af5ec506315761dafc606af5c86f
victor politano <viant3ucf@gmail.com>
2019-11-18 17:44:03 -0500

    styling login

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -48,7 +48,7 @@ return (
       <div className="container matrix">
         <div className="row">
           <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
+            <Link to="/" className="white-text">
               <i className="material-icons left">keyboard_backspace</i> Back to
               home
             </Link>
